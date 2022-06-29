7a21d92efceeebc7f25a979625d3f0dd22c35925
Nakul Bhagwane <nakulbhagwane8413@gmai.com>
2022-06-29 03:46:19 -0700

    initial commit with version 1

---
+++ README.md
@@ -0,0 +1,5 @@
+Hello everyone Nakul here
+
+i am a delhi based front-end web developer and this my first github project 
+
+this is a multipage static website and the idea of this website is its a kind of business website where the owner sell five type of mangos and also give some extra value to theire customer by giving them knowledge about the mangos in articles section and also sharing some best recipes in recipes section
---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById("back_to_top").
+addEventListener('click',()=>{
+    window.scrollTo(0,0)
+});
+
+
+document.getElementById('buynow_banner').addEventListener('click',()=>{
+    window.location="deals.html";
+});
+
+document.getElementById('buynow_c_one').addEventListener('click',()=>{
+    window.location="deals.html";
+});
+
+document.getElementById('buynow_c_two').addEventListener('click',()=>{
+    window.location="deals.html";
+});
+
+document.getElementById('buynow_c_three').addEventListener('click',()=>{
+    window.location="deals.html";
+});
+
+document.getElementById('buynow_c_four').addEventListener('click',()=>{
+    window.location="deals.html";
+});
